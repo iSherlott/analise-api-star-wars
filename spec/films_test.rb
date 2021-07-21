01c34da1cedf95897176d945f34fe6c584271270
iSherlott <sherlott@live.com>
2021-07-20 23:20:06 -0300

    Test #2 Finalizado

--- spec/films_test.rb
+++ spec/films_test.rb
@@ -1,8 +1,15 @@
 require 'httparty'
 
 RSpec.describe 'Validar o formato da request (json válido) para a API' do
-    it 'o header deve retornar application/json se a api retorna um json válido' do
-        response = HttParty.get('films/?format=json')
-        expect(response.header['content-type'] == 'application/json')
+    it 'o header deve retornar application/json se a api retorna um json válido.' do
+        response = HttParty.get('/films/?format=json')
+        expect(response.header['content-type']).to eq('application/json')
+    end
+end
+
+RSpec.describe 'Validar se o retorno HTTP é válido para um GET' do
+    it 'o retorno do HTTP é um GET.' do
+        response = HttParty.get('/films/?format=json')
+        expect(response.code).to eq(200)
     end
 end
