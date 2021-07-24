30a60a747ad2809656d043980da3f75b2b256628
iSherlott <sherlott@live.com>
2021-07-24 13:43:21 -0300

    Test #6 Finalizado

--- README.md
+++ README.md
@@ -30,16 +30,16 @@ etapa: Criado o diario de bordo do checkpoint do git, trocado o final dos arquiv
 
 id: Test #5 Finalizado
 etapa: Finalizado o teste de mesa da quinta etapa.
+
+id: Test #6 Finalizado
+etapa: Finalizado o teste de mesa da sexta etapa.
 ```
 
 ## Biography
 
 <a href="https://www.rubydoc.info/gems/rspec-expectations/frames">rspec expectations</a>
-
 <a href="https://www.udemy.com/course/curso-de-testes-ruby-com-rspec">Curso de Testes para Ruby on Rails com RSpec - Udemy</a>
-
 <a href="https://br.sensedia.com/post/rest-api-understand-the-step-by-step-to-perform-tests">rest api understand the step by step to perform tests<a>
-
 <a href="https://relishapp.com/rspec/rspec-core/v/3-8/docs/filtering/conditional-filters">conditional filters - RSpec</a>
-
 <a href="https://www.youtube.com/watch?v=oIN1EIrebVA&ab_channel=ProgramadorShowzim">Introdução ao TDD com Ruby on Rails e Rspec</a>
+<a href="https://stackoverflow.com/questions/24980295/strictly-convert-string-to-integer-or-nil">strictly convert string to integer or nil</a>
--- spec/films_spec.rb
+++ spec/films_spec.rb
@@ -13,10 +13,10 @@ RSpec.describe 'Validando a rota de filmes', :type => :request do
         end
     end
 
-    context 'Valida se o filme 10 é válido e qual o tipo de retorno ao consultar', :type => :request do
+    context 'Validando se o filme 10 é válido e qual o tipo de retorno ao consultar', :type => :request do
         let (:response) {HttParty.get('/films/10?format=json')}
 
-        it 'O filme 10 é valido' do
+        it 'Validando o filme 10' do
             if response.code == 200
                 expect(response.code).to eq(200)
             else
@@ -25,12 +25,21 @@ RSpec.describe 'Validando a rota de filmes', :type => :request do
         end
     end
 
-    context 'Validar o nome correto de um determinado episódio de filme', :type => :request do
+    context 'Validando o nome correto de um determinado episódio de filme', :type => :request do
         let (:response) {HttParty.get('/films/2?format=json')}
         title = 'The Empire Strikes Back'
 
-        it 'O nome ' + title + ' está valido' do
+        it 'Validando o nome ' + title do
             expect(response['title']).to eq(title)
+        end
+    end
+
+    context 'Validando o ID do episódio e o tipo do dado se está correto', :type => :request do
+        let (:response) {HttParty.get('/films/2?format=json')}
+        id = 5
+
+        it 'Validando o ID: ' + id.to_s do
+            expect(response['episode_id']).to eql(id)
         end
     end
 end
